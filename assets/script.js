a41b555de20df366a5cda635fda2391c6c1c4ed9
framelelo <framelelo89@gmail.com>
2023-11-17 15:27:57 +0400

    Integration Homepage

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,41 @@
-const navBar = document.querySelector('body');
+const bodyContainer = document.querySelector('body');
 const openNav = document.querySelector('.open-nav');
 const closeNav = document.querySelector('.close-nav');
 
 function openNavBar() {
-    navBar.classList.add("open");
+    bodyContainer.classList.add("open");
 }
 function closeNavBar() {
-    navBar.classList.remove("open");
+    bodyContainer.classList.remove("open");
 }
 
 
 openNav.addEventListener('click', openNavBar);
 closeNav.addEventListener('click', closeNavBar);
+
+
+const selectBtn = document.querySelector('.filter');
+const selectedInput= document.querySelectorAll('.options');
+const selectOptions = document.querySelector('.select-dropdown');
+
+function toggleSelectOptions() {
+    if (selectOptions.classList.contains("open-options")) {
+        selectOptions.classList.remove("open-options");
+    } else {
+        selectOptions.classList.add("open-options");
+    }
+}
+
+selectedInput.forEach(function (input) {
+    input.addEventListener('click', toggleSelectOptions);
+});
+
+selectBtn.addEventListener('click', toggleSelectOptions);
+
+document.addEventListener('click', function (event) {
+    const isClickInsideFilter = selectOptions.contains(event.target) || selectBtn.contains(event.target);
+    if (!isClickInsideFilter) {
+        selectOptions.classList.remove("open-options");
+    }
+});
+
